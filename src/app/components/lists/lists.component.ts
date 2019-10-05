5faa1e35a48804ee0535601af823c885a9f00044
David Montalvillo <ddmontal@outlook.com>
2019-10-05 16:40:29 +0200

    feat: Implement edit list name

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, IonList } from '@ionic/angular';
 import { List } from 'src/app/models/list.model';
+import { ListItem } from 'src/app/models/list-item.model';
 
 @Component({
   selector: 'app-lists',
@@ -10,6 +11,7 @@ import { List } from 'src/app/models/list.model';
   styleUrls: ['./lists.component.scss']
 })
 export class ListsComponent implements OnInit {
+  @ViewChild(IonList, {static: false}) list: IonList;
   @Input() done: boolean;
   constructor(public todoService: TodoService, private router: Router, private alertController: AlertController) {}
 
@@ -21,5 +23,39 @@ export class ListsComponent implements OnInit {
 
   deleteList(list: List) {
     this.todoService.deleteList(list);
+  }
+
+  async editListName(list: List) {
+    // this.router.navigateByUrl('/tabs/tab1/new');
+    const alert = await this.alertController.create({
+      header: 'Edit list',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          value: list.title,
+          placeholder: 'List name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.list.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Edit',
+          handler: data => {
+            list.title = data.title;
+            this.todoService.saveToStorage();
+            this.list.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
